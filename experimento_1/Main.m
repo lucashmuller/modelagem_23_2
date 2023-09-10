744486382ebee79f8420b00eee013884ff365a31
Mickael <56806219+MickaelUFSC@users.noreply.github.com>
2023-09-10 10:25:21 -0300

    Update Main.m
    Inclui a configuração de tela.

--- experimento_1/Main.m
+++ experimento_1/Main.m
@@ -1,6 +1,19 @@
 clear
+close all
 clc;
-run("Dados.m");
-run("Resolucao_fzero.m");
-run("Resolucao_subrelaxamento.m");
-run("Graficos.m");
+
+%% Configuração de um grid na tela para gráficos
+
+screenSize = get(0,'screensize'); % gets screen size
+monWidth = screenSize(3);
+monHeight = screenSize(4);
+
+% establishing a 2x2 grid on the screen
+figHeight = monHeight/2;
+figWidth = monWidth/2;
+
+%% Inicializando os arquivos
+run("Modelagem Exp1\Dados.m");
+run("Modelagem Exp1\Resolucao_fzero.m");
+run("Modelagem Exp1\Resolucao_subrelaxamento.m");
+run("Modelagem Exp1\Graficos.m");
